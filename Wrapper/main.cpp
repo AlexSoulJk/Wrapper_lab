44104d45487ab615dd47e9215eba404f6acd17b7
AlexSoulJk <alex.solomatow2016@yandex.ru>
2023-12-03 02:42:52 +0300

    refactor main that's suit tz

--- Wrapper/main.cpp
+++ Wrapper/main.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
-
+#include "Subj.h"
+#include "Wrapper.h"
+#include "Engine.h"
 int main() {
-	return 0;
+    Subj subj;
+    try {
+        Wrapper<int> wrapper(&subj, &Subj::summ, { {"arg1", 0}, {"arg2", 0} });
+        Engine engine;
+        engine.register_command<int>(&wrapper, "summ");
+        std::cout << engine.execute<int>("summ", { {"arg1", 6}, {"arg2", 10} }) << std::endl;
+        Wrapper<std::string> wrapperS(&subj, & Subj::getString);
+        engine.register_command<double>(&wrapperS, "getString", {});
+        std::cout << engine.execute<std::string>("getString", {}) << std::endl;
+    }
+    catch (std::runtime_error const& e) {
+        std::cout << e.what() << std::endl;
+    }
+    
+
+    return 0;
 }
