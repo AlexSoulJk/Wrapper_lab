c3baa8372acb3175342f6383c4bdcfbe697c5b5a
AlexSoulJk <alex.solomatow2016@yandex.ru>
2023-12-03 03:21:56 +0300

    Wrapper Add body of constructor, and call_method

--- Wrapper/Wrapper.h
+++ Wrapper/Wrapper.h
@@ -11,12 +11,25 @@ class Wrapper {
     using DefaultArguments = std::map<std::string, T>;
     using InputArguments = std::map<std::string, T>;
     template<typename Object, typename... Args>
-    using ObjectMethod = T(Object::*func)(Args...);
+    using ObjectMethod = T(Object::*)(Args...);
 public:
-    template<typename Object, typename... Args>
-    Wrapper(Object* object, ObjectMethod<Object,Args...> method, DefaultArguments const& Args);
 
-    T execute(InputArguments const& Args);
+    template<typename Object, typename... Args>
+    Wrapper(Object* object, ObjectMethod<Object, Args...> method, DefaultArguments const& inputArgs) {
+        if (inputArgs.size() != sizeof ...(Args))
+        {
+            throw std::runtime_error("Wrapper_ERROR: The number of default arguments entered does not match the number of arguments for calling the method.");
+        }
+        args = inputArgs;
+        command = [this, object, method](std::vector<T> argsForFunc) {
+            return call_method(object, method, argsForFunc, std::make_index_sequence<sizeof...(Args)>{});
+        };
+    }
+
+    T execute(InputArguments const& Args) {
+        std::vector<T> argsVec;
+        return command(argsVec);
+    }
 
     Wrapper() = default;
     Wrapper(Wrapper const&) = delete;
@@ -26,6 +39,10 @@ public:
     ~Wrapper() = default;
 
 private:
+    template<typename Object, typename Method, size_t... I>
+    T call_method(Object* object, Method method, std::vector<T> inArgs, std::index_sequence<I...>) {
+        return ((object->*method)(inArgs[I]...));
+    }
     Command command;
     DefaultArguments args;
 };
